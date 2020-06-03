10a06ef8ef2615dcda4e03f892a7f5467b8b9a7f
Shalom-Chianu <shalomchianu@gmail.com>
2020-06-02 21:26:36 -0400

    Login and Register pages more or less completed

--- src/components/LoginComponent/Login.js
+++ src/components/LoginComponent/Login.js
@@ -1,14 +1,79 @@
 import React, { useState } from 'react';
 import './Login.css';
+import axios from 'axios';
+
 
 function Login() {
   
   // Declare a new state variable
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(""); // doubles as username for now
+  const [regularUsers, setRegularUsers] = useState([]);
+
+  const getAccountByEmail = (email) => {
+    axios.post("http://localhost:8080/getAccountByEmail/" + "?" + "email=" +  email).then(res => {
+        console.log(res);
+    }).catch(e => {
+        console.log(e);
+        console.log(email);
+    })
+  }
+
+  const getAllRegularUsers = () => {
+    let promise = axios.get("http://localhost:8080/getAllRegularUsers/").then(res => {
+        setRegularUsers([...res.data]);
+     }).catch(e => {
+         console.log(e);
+         return Promise.reject;
+     })
+     return promise;
+  }
+
+  const accountValidation = (email) => {
+      let included = false;
+      let email1 = "";
+      let username = "";
+
+      console.log(regularUsers);
+
+      // may need to change the username = password thing -> for testing purposes
+      for (const ele of regularUsers) {
+        if (ele.accountDTO.email == email && ele.accountDTO.username == password) {
+            included = true; 
+            email1 = ele.accountDTO.email;
+            username = ele.accountDTO.username;
+            break;
+        }
+      }
+
+      if (included) {
+          getAccountByEmail(email); // how do we go from here to the next page
+      } else {
+        console.log("email1: " + email1);
+        console.log("email: " + email);
+     //   throw new Error("Email or password may be incorrect");
+    }
+  }
+
+  const handleSubmit = (email) => {
+        getAllRegularUsers().then( e => {
+            accountValidation(email)
+        }).catch(e => {
+            console.log(e);
+        })
+  }
   
   return (
     <div className="root-container">
+
+        <div className="title">
+            <h1>Note-Taking App</h1>
+        </div>
+
+        <div className="subtitle">
+            <p>(Subtitle)</p>
+        </div>
+        
         <div className="box-container">
             <div className="inner-container">
                 <div className="header">
@@ -36,7 +101,13 @@ function Login() {
 
                 <button
                     type="button"
-                    className="login-btn">Login</button> 
+                    className="login-btn"
+                    onClick = { () => handleSubmit(email)}>Login
+                </button> 
+
+                <div>
+                    <p className = "message">Don't have an account? <a href="#">Register.</a></p> {/*How to use Link and then style it*/}
+                </div>
 
                 </div>
             </div>
